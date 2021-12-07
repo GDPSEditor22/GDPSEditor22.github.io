496695fee787e0d00e605ed27ae791efe574fca0
CarlosPreGD <69406375+CarlosPreGD@users.noreply.github.com>
2021-12-07 11:40:04 -0500

    Add files via upload

---
+++ betaTools/changelog.php
@@ -0,0 +1,7 @@
+#editorBeta2.2.0.2#
+- Fixed logo size for long screens
+- Added version name
+- Fixed the color of the "tools" logo, now it's random
+- Fixed button position (now it's exactly in the middle)
+- Embed fixed
+- Optimized code and page
